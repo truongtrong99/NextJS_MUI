d299be1064fe0565e3b546815d8b04141869f25c
truongtrong99 <truongtrong400@gmail.com>
2025-09-11 11:31:27 +0700

    #102 - Fetch Data HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,18 @@ import AppHeader from "@/components/header/app.header";
 import MainSlider from "@/components/main/main.slider";
 import { Container } from "@mui/material";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 10
+    })
+  });
+  console.log(await res.json());
   return (
     <Container>
       <MainSlider />
