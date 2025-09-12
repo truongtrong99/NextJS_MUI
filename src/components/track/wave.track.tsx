1ab7ac00d74bf168412c2fbf1ee71c0fe09ed0be
truongtrong99 <truongtrong400@gmail.com>
2025-09-12 15:50:35 +0700

    #112 - WaveSurfer Hook

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,24 +1,57 @@
 'use client'
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import WaveSurfer from 'wavesurfer.js';
+//WaveSurfer hook
+const useWaveSurfer = (containerRef: any, options: any) => {
+    const [wavesurfer, setWavesurfer] = useState<any>(null);
+
+    useEffect(() => {
+        if (!containerRef.current) return;
+
+        const ws = WaveSurfer.create({
+            ...options,
+            container: containerRef.current,
+        });
+
+        setWavesurfer(ws);
+
+        return () => {
+            ws.destroy();
+        };
+    }, [containerRef, options]);
+
+    return wavesurfer;
+}
 
 const WaveTrack = () => {
     const searchParams = useSearchParams()
     const fileName = searchParams.get('audio')
     const containerRef = useRef<HTMLDivElement | null>(null);
-
-    useEffect(() => {
-        if (containerRef.current) {
-            const wavesurfer = WaveSurfer.create({
-                container: containerRef.current,
-                waveColor: 'rgb(200, 0, 200)',
-                progressColor: 'rgb(100, 0, 100)',
-                url: `/api?audio=${fileName}`,
-            })
+    const optionsMemo = useMemo(() => {
+        return {
+            waveColor: 'rgb(200, 0, 200)',
+            progressColor: 'rgb(100, 0, 100)',
+            url: `/api?audio=${fileName}`,
         }
-
     }, [])
+    // const options = {
+    //     waveColor: 'rgb(200, 0, 200)',
+    //     progressColor: 'rgb(100, 0, 100)',
+    //     url: `/api?audio=${fileName}`,
+    // }
+    const wavesurfer = useWaveSurfer(containerRef, optionsMemo);
+    // useEffect(() => {
+    //     if (containerRef.current) {
+    //         const wavesurfer = WaveSurfer.create({
+    //             container: containerRef.current,
+    //             waveColor: 'rgb(200, 0, 200)',
+    //             progressColor: 'rgb(100, 0, 100)',
+    //             url: `/api?audio=${fileName}`,
+    //         })
+    //     }
+
+    // }, [])
     return <div ref={containerRef}>WaveTrack</div>;
 }
 export default WaveTrack;
