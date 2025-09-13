04662ba525155224999ed8a72f7647ede75f3521
truongtrong99 <truongtrong400@gmail.com>
2025-09-13 10:32:10 +0700

    #117 - Add Gradients

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -10,11 +10,33 @@ const WaveTrack = () => {
     const fileName = searchParams.get('audio')
     const containerRef = useRef<HTMLDivElement | null>(null);
     const optionsMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')!
+
+        // Define the waveform gradient
+        const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        gradient.addColorStop(0, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
+        gradient.addColorStop(1, '#B1B1B1') // Bottom color
+
+        // Define the progress gradient
+        const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        progressGradient.addColorStop(0, '#EE772F') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
+        progressGradient.addColorStop(1, '#F6B094') // Bottom color
+
         return {
-            waveColor: 'rgb(200, 0, 200)',
-            progressColor: 'rgb(100, 0, 100)',
+            waveColor: gradient,
+            progressColor: progressGradient,
             url: `/api?audio=${fileName}`,
             barWidth: 2,
+            height: 150,
         }
     }, [])
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
