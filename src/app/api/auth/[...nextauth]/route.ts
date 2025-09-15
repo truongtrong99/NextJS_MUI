4731cb82b6e3af59e339857549a332bec69ba0d6
truongtrong99 <truongtrong400@gmail.com>
2025-09-15 16:44:41 +0700

    #133 - Implement GitHub sign-in callback to retrieve access and refresh tokens

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { AuthOptions } from "next-auth"
+import { sendRequest } from "@/utils/api"
+import { JWT } from "next-auth/jwt"
 export const authOptions: AuthOptions = {
     // Configure one or more authentication providers
     secret: process.env.NO_SECRET,
@@ -13,12 +15,28 @@ export const authOptions: AuthOptions = {
     callbacks: {
         jwt: async ({ token, user, account, profile, trigger }) => {
             if (trigger === 'signIn' && account?.provider === 'github') {
-
+                const res = await sendRequest<IBackendRes<JWT>>({
+                    url: "http://localhost:8000/api/v1/auth/social-media",
+                    method: "POST",
+                    body: {
+                        type: 'GITHUB',
+                        username: user.email
+                    }
+                })
+                if (res?.data) {
+                    token.access_token = res.data.access_token
+                    token.refresh_token = res.data.refresh_token
+                    token.user = res.data.user
+                }
             }
             return token
         },
         async session({ session, token, user }) {
-            // session.user.address = token.address as any;
+            if (token) {
+                session.access_token = token.access_token
+                session.refresh_token = token.refresh_token
+                session.user = token.user
+            }
             return session
         },
     }
