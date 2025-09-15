6f22aad87d5d85bcce68b47ec31d259f38f5d877
truongtrong99 <truongtrong400@gmail.com>
2025-09-15 17:12:39 +0700

    #134 - Login With Credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,44 @@ import GithubProvider from "next-auth/providers/github"
 import { AuthOptions } from "next-auth"
 import { sendRequest } from "@/utils/api"
 import { JWT } from "next-auth/jwt"
+import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: AuthOptions = {
     // Configure one or more authentication providers
     secret: process.env.NO_SECRET,
     providers: [
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "Credentials",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const res = await sendRequest<IBackendRes<any>>({
+                    url: "http://localhost:8000/api/v1/auth/login",
+                    method: "POST",
+                    body: {
+                        username: credentials?.username,
+                        password: credentials?.password
+                    }
+                })
+
+                if (res && res.data) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return res.data
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
@@ -14,12 +48,12 @@ export const authOptions: AuthOptions = {
     ],
     callbacks: {
         jwt: async ({ token, user, account, profile, trigger }) => {
-            if (trigger === 'signIn' && account?.provider === 'github') {
+            if (trigger === 'signIn' && account?.provider !== 'credentials') {
                 const res = await sendRequest<IBackendRes<JWT>>({
                     url: "http://localhost:8000/api/v1/auth/social-media",
                     method: "POST",
                     body: {
-                        type: 'GITHUB',
+                        type: account?.provider?.toUpperCase(),
                         username: user.email
                     }
                 })
@@ -28,6 +62,14 @@ export const authOptions: AuthOptions = {
                     token.refresh_token = res.data.refresh_token
                     token.user = res.data.user
                 }
+            }
+            if (trigger === 'signIn' && account?.provider === 'credentials') {
+                //@ts-ignore
+                token.access_token = user.access_token
+                //@ts-ignore
+                token.refresh_token = user.refresh_token
+                //@ts-ignore
+                token.user = user.user
             }
             return token
         },
